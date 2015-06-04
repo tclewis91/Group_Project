8aef35c6120853fac53846fad571577edc4083cb
tclewis91 <tclewis91@gmail.com>
2015-06-04 16:11:31 -0500

    new users

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,3 +1,18 @@
 class TrainersController < ApplicationController
+  def index
+    render json: Trainer.all
+  end
 
+  def new
+    render json: Trainer.new
+  end
+
+  def create
+    begin
+      poke_ball = Trainer.create(name: params(:name),message: params(:message),badge: params(:badge))
+      render json: poke_ball
+    rescue ActionController::ParameterMissing => error
+      render json: { error: error.message }, status 422
+    end
+  end
 end
