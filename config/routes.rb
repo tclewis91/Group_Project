cd7470e1fc073a386a6b1b98861c0a62cfa32959
tclewis91 <tclewis91@gmail.com>
2015-06-04 17:04:52 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+get'/trainers', to: 'trainers#index'
+post'trainers/', to: 'trainers#create'
+put '/trainers/:name', to: 'trainers#update'
+root 'trainers#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
