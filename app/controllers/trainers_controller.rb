65accd4ad6d58a07dd0a9637e58668ca7c649c92
tclewis91 <tclewis91@gmail.com>
2015-06-05 08:23:10 -0500

    syntax

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,24 +2,25 @@ class TrainersController < ApplicationController
   def index
     render json: Trainer.all
   end
-end
+
 
  def show
     render json: Trainer.find(params[:id])
   end
 
-  # def new
-  #   render json: Trainer.new
-  # end
+  def new
+    render json: Trainer.new
+  end
 
   def create
     begin
       poke_ball = Trainer.create(name: params(:name),message: params(:message),badge: params(:badge))
       render json: poke_ball
     rescue ActionController::ParameterMissing => error
-      render json: { error: error.message }, status 422
+      render json: { error: error.message }, status: 422
     end
   end
+end
 
 #   # def time
 #   #   time.strftime("%B %-d, %Y, %H:%M:%S")
