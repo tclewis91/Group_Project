333ebce5dcad7dbdc4ab5291fd222ab272dbe373
tclewis91 <tclewis91@gmail.com>
2015-06-05 07:50:37 -0500

    sq3lite

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,22 +2,23 @@ class TrainersController < ApplicationController
   def index
     render json: Trainer.all
   end
-
-  def new
-    render json: Trainer.new
-  end
-
-  def create
-    begin
-      poke_ball = Trainer.create(name: params(:name),message: params(:message),badge: params(:badge))
-      render json: poke_ball
-    rescue ActionController::ParameterMissing => error
-      render json: { error: error.message }, status 422
-    end
-  end
-
-  def time
-    time.strftime("%B %-d, %Y, %H:%M:%S")
-
-
 end
+
+#   def new
+#     render json: Trainer.new
+#   end
+
+#   def create
+#     begin
+#       poke_ball = Trainer.create(name: params(:name),message: params(:message),badge: params(:badge))
+#       render json: poke_ball
+#     rescue ActionController::ParameterMissing => error
+#       render json: { error: error.message }, status 422
+#     end
+#   end
+
+#   # def time
+#   #   time.strftime("%B %-d, %Y, %H:%M:%S")
+#   # end
+
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+50.times do Trainer.create(name:  Faker::Name.first_name,
+message: Faker::Company.catch_phrase, badge: Faker::Name.digit)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
