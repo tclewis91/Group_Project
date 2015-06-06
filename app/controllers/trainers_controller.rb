031fe88fa66ac2419c1962d992d7664a87fc4e28
tclewis91 <tclewis91@gmail.com>
2015-06-06 08:23:03 -0500

    time

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,17 +1,25 @@
 class TrainersController < ApplicationController
-  def index
-    render json: Trainer.all
+   def time#format time
+    time.now = time.strftime("%B %-d, %Y, %H:%M:%S")
   end
 
- def show
+  def index #homepage
+    clear_board = Trainer.all.select{ |trainer| (time.now - 300) }
+    render json: Trainer.where(created_at: clear_board).order(created_at: :desc)
+  rescue ActionController::RecordNotFound => error, status: 400
+      render json: { error: error.message }
+    end
+  end
+
+ def show #if you need to find a user
     render json: Trainer.find(params[:id])
+  rescue ActionController::RecordNotFound => error
+      render json: { error: error.message }, status: 400
+    end
   end
 
-  def new
-    render json: Trainer.new
-  end
 
-  def create
+  def create #creates new user
     begin
       poke_ball = Trainer.create(name: params.fetch(:name), message: params.fetch(:message), badge: params.fetch(:badge))
       render json: poke_ball
@@ -21,8 +29,4 @@ class TrainersController < ApplicationController
   end
 end
 
-
-#   # def time
-#   #   time.strftime("%B %-d, %Y, %H:%M:%S")
-#   # end
 
